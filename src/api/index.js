a16640ac10283fbc6463c8118299df9eddce1746
eransafadel <eransafadel@gmail.com>
2022-08-04 18:58:02 +0300

    example commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:5000',
+const API =  axios.create({ baseURL: 'http://localhost:5000',
 timeout: 2000,
 headers: {'Content-Type': 'application/json'}
 
 }
 
 );
+console.log("111")
 
 export const checkoutPaymentPost = (tokenId,amount) => API.post('/api/checkout/payment',{tokenId,amount});
